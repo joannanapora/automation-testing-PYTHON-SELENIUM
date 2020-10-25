c239f1cfa2d529ff423e04d28e08d211736e31f5
porczi <porczi@MacBook-Air.local>
2020-10-25 16:01:26 +0000

    feat: add saving to database

--- selenka.py
+++ selenka.py
@@ -1,10 +1,9 @@
-from __future__ import unicode_literals
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-
+import pymysql.cursors
 import unittest
 
 # export PATH=$PATH:/Users/porczi/Downloads/chromedriver
@@ -22,6 +21,31 @@ class oto_moto_search(unittest.TestCase):
     def wait_until_clickable(self, ByFn, path):
         self.wait.until(EC.element_to_be_clickable(
             (ByFn, path)))
+
+    def get_quote_details(self, quote):
+
+        price_details = quote.find_element_by_class_name(
+            "offer-item__price")
+        price = price_details.find_element_by_xpath(
+            ".//span[1]/span[1]").text
+        price = price.replace(" ", "")
+
+        title = quote.find_element_by_xpath(
+            ".//div[@class='offer-item__title']/h2").text
+
+        ID = quote.find_element_by_xpath(
+            ".//div[@class='offer-item__title']/h2/a").get_attribute("data-ad-id")
+
+        img_src = quote.find_element_by_xpath(
+            ".//div[@class='offer-item__photo  ds-photo-container']/a/img").get_attribute("src")
+        location = quote.find_element_by_xpath(
+            ".//div[@class='offer-item__content ds-details-container']/h4/span[2]").text
+        region = quote.find_element_by_xpath(
+            ".//div[@class='offer-item__content ds-details-container']/h4/span[3]").text
+        region = region.replace("(", "")
+        region = region.replace(")", "")
+
+        return {"title": title, "price": price, "ID": ID, "img_src": img_src, "location": location, "region": region}
 
     def setUp(self):
         self.driver = Chrome()
@@ -119,6 +143,38 @@ class oto_moto_search(unittest.TestCase):
 
         self.assertLessEqual(int(price), int(price_before_sort))
         self.assertLessEqual(int(price), int(price_two))
+
+        # all quotes on page \/\/\/
+        quotes = driver.find_elements_by_xpath(
+            "//div[@class='offers list']/article")
+
+        quotes_details = []
+
+        for i in range(0, 3):
+            details = self.get_quote_details(quotes[i])
+            quotes_details.append(details)
+
+        # CONNECTING TO dockerMySql \/\/\/:
+
+        connection = pymysql.connect(host='localhost',
+                                     user='sara',
+                                     password='sarka',
+                                     db='OtoMoto',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+
+        try:
+            with connection.cursor() as cursor:
+                for i in quotes_details:
+                    sql = "CREATE TABLE IF NOT EXISTS OtoMoto.bmw (title varchar(50) , PLN varchar(50), id varchar(50), location varchar(50), region varchar(50), img varchar(500))"
+                    cursor.execute(sql)
+                    # Create a new record
+                    sql = "INSERT INTO OtoMoto.bmw (title, PLN, id, location, region, img) VALUES (%s, %s, %s, %s, %s, %s)"
+                    cursor.execute(
+                        sql, (i["title"], i["price"], i["ID"], i["location"], i["region"], i["img_src"]))
+                    connection.commit()
+        finally:
+            connection.close()
 
     def tear_down(self):
         self.driver.quit()
